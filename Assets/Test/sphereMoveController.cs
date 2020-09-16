89748c846fa2ebb86fcf2c3254ef27e09de677cc
liziteng <liziteng@qq.com>
2020-09-16 10:53:33 +0800

    简要说明：场景当中蓝色小球可以使用“W、A、S、D”移动。 用小球撞门实现开关门的效果。 问题内容：如何让红色的门关闭时停留在门框内。 （文件里面的Test文件夹不要修改，制作时创建自己的文件夹）

---
+++ Assets/Test/sphereMoveController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphereMoveController : MonoBehaviour
+{
+    Rigidbody rigibody;
+
+    private void Start()
+    {
+        rigibody = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        InputSystem(KeyCode.W, new Vector3(-1, 0,0));
+        InputSystem(KeyCode.A, new Vector3(0, 0, -1));
+        InputSystem(KeyCode.S, new Vector3(1, 0, 0));
+        InputSystem(KeyCode.D, new Vector3(0, 0, 1));
+    }
+
+    void InputSystem(KeyCode key, Vector3 vect3)
+    {
+        if(Input.GetKey(key))
+        {
+            rigibody.AddForce(vect3);
+        }
+    }
+
+}
